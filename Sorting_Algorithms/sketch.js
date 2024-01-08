c089a39ddaf93726bb41119e5b73a201c9f8f524
tanmay1501 <66867695+tanmay1501@users.noreply.github.com>
2024-01-08 15:48:29 -0600

    second commit

--- Sorting_Algorithms/sketch.js
+++ Sorting_Algorithms/sketch.js
@@ -1,13 +1,62 @@
 // sketch.js
+let values =[];
+let len =100
+let i =0
+let j= 0
 function setup() {
     createCanvas(1200, 600); // Set the size of the canvas
+    for(let i = 0; i < len ; i++){
+      values.push(random(height));
   }
+  textSize(40);
+}
   
   function draw() {
-    background(255); // Set the background color
+    // Set the background color
+    background(125)
+    fill(0);
+    text('Bubble Sort', 20, 50);
+    bubblesort();
+    simulatesort();
+
+  }
   
-    // Your drawing code goes here
-    fill(255, 0, 0); // Set fill color to red
-    ellipse(width / 2, height / 2, 50, 50); // Draw a red circle at the center
+  function bubblesort(){
+    if(i < len){
+      
+    for(let k =0 ; k<1;k++){          
+      if(values[j] > values[j+1]){
+        let temp = values[j]
+        values[j] = values[j+1]
+        values[j+1] = temp
+      }
+      j++
+      if(j== len - i-1){
+        j =0
+        i ++;
+      }
+    }
+    
+  }
+  else{
+      noLoop();
+    }
+  }
+  
+  function simulatesort()
+  { 
+    
+    
+    for(let x = 0; x <= len;x++)
+    {
+      b = width/len
+      stroke(0);
+      if(x == j || x >= len - i-1)
+        fill(255,0,0)
+      else
+        fill(255,105,0);
+      rect(x*b,height-values[x],b,values[x],20);
+    }
+    
   }
   
