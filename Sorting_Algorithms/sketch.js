fd6fb7cc3150884faa67a210a5cf3f1887486987
tanmay1501 <66867695+tanmay1501@users.noreply.github.com>
2024-01-07 23:20:13 -0600

    initial commit
    written basic code

---
+++ Sorting_Algorithms/sketch.js
@@ -0,0 +1,13 @@
+// sketch.js
+function setup() {
+    createCanvas(1200, 600); // Set the size of the canvas
+  }
+  
+  function draw() {
+    background(255); // Set the background color
+  
+    // Your drawing code goes here
+    fill(255, 0, 0); // Set fill color to red
+    ellipse(width / 2, height / 2, 50, 50); // Draw a red circle at the center
+  }
+  
